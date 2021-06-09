80ee2387288f93a07194d8a71e53cd57082d7014
Habibun Noby <habibun.noby@gmail.com>
2021-06-09 20:43:14 +0600

    refactor(solid): update dependency inversion principle

--- design-principle/SOLID/dependency_inversion.php
+++ design-principle/SOLID/dependency_inversion.php
@@ -1,11 +1,9 @@
 <?php
-//Dependency Inversion Principle
-
 /**
- * Rules
+ * Dependency inversion principle
  *
- * 1. High-level modules should not depend on low-level modules. Both should depend on abstractions (e.g. interfaces).
- * 2. Abstractions should not depend on details. Details (concrete implementations) should depend on abstractions.
+ * High-level modules should not depend on low-level modules. Both should depend on abstractions (e.g. interfaces).
+ * Abstractions should not depend on details. Details (concrete implementations) should depend on abstractions.
  */
 
 class MainApplication {
