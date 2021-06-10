0bda8f7851025f71489b5ca6e7a13da0114f1e69
Habibun Noby <habibun.noby@gmail.com>
2021-06-10 13:34:31 +0600

    feat(php7.4): contravariance

---
+++ core/7.3.x->7.4.x/contravariance.php
@@ -0,0 +1,27 @@
+<?php
+
+class Food {}
+
+class AnimalFood extends Food {}
+
+abstract class Animal
+{
+    protected string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function eat(AnimalFood $food)
+    {
+        echo $this->name . " eats " . get_class($food);
+    }
+}
+
+class Dog extends Animal
+{
+    public function eat(Food $food) {
+        echo $this->name . " eats " . get_class($food);
+    }
+}
