e09da7921a3cb9a0782225865425ae5a670c4adc
Habibun Noby <habibun.noby@gmail.com>
2021-06-12 18:02:02 +0600

    feat(php7.1): add nullable types

---
+++ core/7.0.x->7.1.x/nullable_types.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Nullable types ¶
+ *
+ * Type declarations for parameters and return values can now be marked as nullable by prefixing the type name with a question mark.
+ * This signifies that as well as the specified type, null can be passed as an argument, or returned as a value, respectively.
+ */
+
+function testReturn(): ?string
+{
+    return null;
+}
+
+$res = testReturn();
+
+var_export($res);
