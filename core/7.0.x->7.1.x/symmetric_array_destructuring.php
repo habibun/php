e864ca23b52fd76bd48f31bffc171315d33816e1
Habibun Noby <habibun.noby@gmail.com>
2021-06-16 09:56:35 +0600

    feat(php-7.1): symmetric array destructuring

---
+++ core/7.0.x->7.1.x/symmetric_array_destructuring.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Symmetric array destructuring ¶
+ *
+ * The shorthand array syntax ([]) may now be used to destructure arrays for assignments (including within foreach),
+ * as an alternative to the existing list() syntax, which is still supported.
+ */
+
+$data = [
+    [1, 'Tom'],
+    [2, 'Fred'],
+];
+
+// list() style
+//list($id1, $name1) = $data[0];
+
+
+// [] style
+//[$id1, $name1] = $data[0];
+
+
+// list() style
+foreach ($data as list($id, $name)) {
+//    var_dump($id, $name);
+}
+
+// [] style
+foreach ($data as [$id, $name]) {
+    var_dump($id, $name);
+}
+
+
