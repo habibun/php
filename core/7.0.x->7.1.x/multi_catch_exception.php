818936f58b2222a2ee78b1ace3e0d4ae4b579f8a
Habibun Noby <habibun.noby@gmail.com>
2021-06-16 10:16:36 +0600

    feat(php-7.1): add multi catch exception

---
+++ core/7.0.x->7.1.x/multi_catch_exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Multi catch exception handling ¶
+ *
+ * Multiple exceptions per catch block may now be specified using the pipe character (|).
+ * This is useful for when different exceptions from different class hierarchies are handled the same.
+ */
+
+try {
+    // some code
+} catch (FirstException | SecondException $e) {
+    // handle first and second exceptions
+}
