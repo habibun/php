591f3ad29051e2a20e514de3db08a8c2f2097f35
Habibun Noby <habibun.noby@gmail.com>
2021-06-16 18:19:22 +0600

    feat(function): add trim

---
+++ core/function/trim.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * trim
+ *
+ * Strip whitespace (or other characters) from the beginning and end of a string
+ */
+
+$str = "Hello World!";
+echo $str . "<br>";
+echo trim($str,"Hed!");
