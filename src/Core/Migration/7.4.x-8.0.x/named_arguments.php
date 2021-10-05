f51ffb04c69f4b7e3814c888c3b59e66f176bc5c
Habibun Noby <habibun.noby@gmail.com>
2021-10-05 21:12:11 +0600

    feat(php-8): named argument

--- src/Core/Migration/7.4.x-8.0.x/named_arguments.php
+++ src/Core/Migration/7.4.x-8.0.x/named_arguments.php
@@ -1,1 +1,16 @@
 <?php
+
+function userInfo(string $name, string $email = '', int $age=5)
+{
+    return [
+        'name' => $name,
+        'email' => $email,
+        'age' => $age
+    ];
+}
+
+$result = userInfo(name: 'jony',  age: 20);
+
+echo '<pre>';
+var_dump($result);
+echo '</pre>';
