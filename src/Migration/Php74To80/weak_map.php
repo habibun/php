498d75216c931d4f16e0e8662fc449cbc92e9899
Habibun Noby <habibun.noby@gmail.com>
2021-10-09 10:35:31 +0600

    feat(php8): add weakmap

--- src/Migration/Php74To80/weak_map.php
+++ src/Migration/Php74To80/weak_map.php
@@ -1,4 +1,27 @@
 <?php
-namespace Php\Migration\Php74To80;
-//todo - later
 
+namespace Php\Migration\Php74To80;
+
+$map = new \splObjectStorage();
+
+$object = new \stdClass();
+$map[$object] = 'Foo';
+
+var_dump(count($map)); // int(1)
+
+unset($object);
+
+var_dump(count($map)); // int(1)
+
+//the references are weak, and the reference inside the WeakMap does not prevent
+// the garbage collector from clearing the object if there are no other references to the object.
+$map = new \WeakMap();
+
+$object = new \stdClass();
+$map[$object] = 'Foo';
+
+var_dump(count($map)); // int(1)
+
+unset($object);
+
+var_dump(count($map)); // int(1)
