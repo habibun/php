d9dc62d767b3d60774b33e68e506f13b348f1ccd
Habibun Noby <habibun.noby@gmail.com>
2021-10-11 06:24:59 +0600

    feat: scalar type declarations

--- src/Migration/Php56To70/scalar_type_declarations.php
+++ src/Migration/Php56To70/scalar_type_declarations.php
@@ -1,4 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Php\Migration\Php56To70;
 
+ini_set('display_errors', 'On');
+
+////Coercive Mode
+//function sum(int ...$ints) {
+//    return array_sum($ints);
+//}
+//print(sum(2, '3', 4.1));
+
+// Strict mode
+function sum(int ...$ints)
+{
+    return array_sum($ints);
+}
+echo sum(2, '3', 4.1);
