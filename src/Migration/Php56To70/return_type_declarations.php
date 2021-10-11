e8bb275b367518bd59f44d7ca58e53a55477009e
Habibun Noby <habibun.noby@gmail.com>
2021-10-11 20:31:59 +0600

    feat(php7): add return type declarations

---
+++ src/Migration/Php56To70/return_type_declarations.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Php\Migration\Php56To70;
+
+function arraysSum(array ...$arrays): array
+{
+    return array_map(function (array $array): int {
+        return array_sum($array);
+    }, $arrays);
+}
+
+print_r(arraysSum([1, 2, 3], [4, 5, 6], [7, 8, 9]));
