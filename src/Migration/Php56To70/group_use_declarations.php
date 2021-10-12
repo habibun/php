6cbde8a17a4c39c701d69276059463cc12bdb1ab
Habibun Noby <habibun.noby@gmail.com>
2021-10-12 06:14:22 +0600

    feat(php7): add group use declarations

---
+++ src/Migration/Php56To70/group_use_declarations.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Php\Migration\Php56To70;
+
+// Pre PHP 7 code
+use some\namespace\ClassA;
+use some\namespace\ClassB;
+use some\namespace\ClassC as C;
+
+// PHP 7+ code
+use some\namespace\{ClassA, ClassB, ClassC as C};
