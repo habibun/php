997c5135f28a24dcab5575ba78a5105d6c38dbcb
Habibun Noby <habibun.noby@gmail.com>
2021-10-12 09:02:27 +0600

    feat(php7): add preg replace callback array

---
+++ src/Migration/Php56To70/preg_replace_callback_array.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Php\Migration\Php56To70;
+
+$subject = 'Aaaaaa Bbb';
+
+$res = preg_replace_callback_array(
+    [
+        '~[a]+~i' => function ($match) {
+            echo strlen($match[0]), ' matches for "a" found', PHP_EOL;
+        },
+        '~[b]+~i' => function ($match) {
+            echo strlen($match[0]), ' matches for "b" found', PHP_EOL;
+        }
+    ],
+    $subject
+);
+
+var_dump($res);
