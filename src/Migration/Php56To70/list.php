3f0b1bfb303f3bdc2270ca4999ba3679ef2fefd5
Habibun Noby <habibun.noby@gmail.com>
2021-10-12 09:07:55 +0600

    feat(php7): add list

---
+++ src/Migration/Php56To70/list.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Php\Migration\Php56To70;
+
+$info = array('coffee', 'brown', 'caffeine');
+list($drink, $color, $power) = $info;
+echo "$drink is $color and $power makes it special.\n";
