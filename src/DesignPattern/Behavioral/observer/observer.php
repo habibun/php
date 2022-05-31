9351d24764985dda7d0bc56c829c481b9beffcb6
Habibun Noby <habibun.noby@gmail.com>
2022-05-31 20:49:59 +0600

    fix: observer

--- src/DesignPattern/Behavioral/observer/observer.php
+++ src/DesignPattern/Behavioral/observer/observer.php
@@ -1,206 +1,95 @@
 <?php
 
-/**
- * The UserRepository represents a Subject. Various objects are interested in
- * tracking its internal state, whether it's adding a new user or removing one.
- */
-class UserRepository implements \SplSubject
+ // Doc: https://sourcemaking.com/design_patterns/observer/php
+
+
+abstract class AbstractObserver
 {
-    /**
-     * @var array The list of users.
-     */
-    private $users = [];
+    abstract function update(AbstractSubject $subject_in);
+}
 
-    // Here goes the actual Observer management infrastructure. Note that it's
-    // not everything that our class is responsible for. Its primary business
-    // logic is listed below these methods.
+abstract class AbstractSubject
+{
+    abstract function attach(AbstractObserver $observer_in);
 
-    /**
-     * @var array
-     */
-    private $observers = [];
+    abstract function detach(AbstractObserver $observer_in);
 
+    abstract function notify();
+}
+
+function writeln($line_in)
+{
+    echo $line_in . "<br/>";
+}
+
+class PatternObserver extends AbstractObserver
+{
     public function __construct()
     {
-        // A special event group for observers that want to listen to all
-        // events.
-        $this->observers["*"] = [];
     }
 
-    private function initEventGroup(string $event = "*"): void
+    public function update(AbstractSubject $subject)
     {
-        if (!isset($this->observers[$event])) {
-            $this->observers[$event] = [];
-        }
+        writeln('*IN PATTERN OBSERVER - NEW PATTERN GOSSIP ALERT*');
+        writeln(' new favorite patterns: ' . $subject->getFavorites());
+        writeln('*IN PATTERN OBSERVER - PATTERN GOSSIP ALERT OVER*');
+    }
+}
+
+class PatternSubject extends AbstractSubject
+{
+    private $favoritePatterns = NULL;
+    private $observers = array();
+
+    function __construct()
+    {
     }
 
-    private function getEventObservers(string $event = "*"): array
+    function attach(AbstractObserver $observer_in)
     {
-        $this->initEventGroup($event);
-        $group = $this->observers[$event];
-        $all = $this->observers["*"];
-
-        return array_merge($group, $all);
+        //could also use array_push($this->observers, $observer_in);
+        $this->observers[] = $observer_in;
     }
 
-    public function attach(\SplObserver $observer, string $event = "*"): void
+    function detach(AbstractObserver $observer_in)
     {
-        $this->initEventGroup($event);
-
-        $this->observers[$event][] = $observer;
-    }
-
-    public function detach(\SplObserver $observer, string $event = "*"): void
-    {
-        foreach ($this->getEventObservers($event) as $key => $s) {
-            if ($s === $observer) {
-                unset($this->observers[$event][$key]);
+        //$key = array_search($observer_in, $this->observers);
+        foreach ($this->observers as $okey => $oval) {
+            if ($oval == $observer_in) {
+                unset($this->observers[$okey]);
             }
         }
     }
 
-    public function notify(string $event = "*", $data = null): void
+    function notify()
     {
-        echo "UserRepository: Broadcasting the '$event' event.\n";
-        foreach ($this->getEventObservers($event) as $observer) {
-            $observer->update($this, $event, $data);
+        foreach ($this->observers as $obs) {
+            $obs->update($this);
         }
     }
 
-    // Here are the methods representing the business logic of the class.
-
-    public function initialize($filename): void
+    function updateFavorites($newFavorites)
     {
-        echo "UserRepository: Loading user records from a file.\n";
-        // ...
-        $this->notify("users:init", $filename);
+        $this->favorites = $newFavorites;
+        $this->notify();
     }
 
-    public function createUser(array $data): User
+    function getFavorites()
     {
-        echo "UserRepository: Creating a user.\n";
-
-        $user = new User();
-        $user->update($data);
-
-        $id = bin2hex(openssl_random_pseudo_bytes(16));
-        $user->update(["id" => $id]);
-        $this->users[$id] = $user;
-
-        $this->notify("users:created", $user);
-
-        return $user;
-    }
-
-    public function updateUser(User $user, array $data): ?User
-    {
-        echo "UserRepository: Updating a user.\n";
-
-        $id = $user->attributes["id"];
-        if (!isset($this->users[$id])) {
-            return null;
-        }
-
-        $user = $this->users[$id];
-        $user->update($data);
-
-        $this->notify("users:updated", $user);
-
-        return $user;
-    }
-
-    public function deleteUser(User $user): void
-    {
-        echo "UserRepository: Deleting a user.\n";
-
-        $id = $user->attributes["id"];
-        if (!isset($this->users[$id])) {
-            return;
-        }
-
-        unset($this->users[$id]);
-
-        $this->notify("users:deleted", $user);
+        return $this->favorites;
     }
 }
 
-/**
- * Let's keep the User class trivial since it's not the focus of our example.
- */
-class User
-{
-    public $attributes = [];
+writeln('BEGIN TESTING OBSERVER PATTERN');
+writeln('');
 
-    public function update($data): void
-    {
-        $this->attributes = array_merge($this->attributes, $data);
-    }
-}
+$patternGossiper = new PatternSubject();
+$patternGossipFan = new PatternObserver();
+$patternGossiper->attach($patternGossipFan);
+$patternGossiper->updateFavorites('abstract factory, decorator, visitor');
+$patternGossiper->updateFavorites('abstract factory, observer, decorator');
+$patternGossiper->detach($patternGossipFan);
+$patternGossiper->updateFavorites('abstract factory, observer, paisley');
 
-/**
- * This Concrete Component logs any events it's subscribed to.
- */
-class Logger implements \SplObserver
-{
-    private $filename;
+writeln('END TESTING OBSERVER PATTERN');
 
-    public function __construct($filename)
-    {
-        $this->filename = $filename;
-        if (file_exists($this->filename)) {
-            unlink($this->filename);
-        }
-    }
-
-    public function update(\SplSubject $repository, string $event = null, $data = null): void
-    {
-        $entry = date("Y-m-d H:i:s") . ": '$event' with data '" . json_encode($data) . "'\n";
-        file_put_contents($this->filename, $entry, FILE_APPEND);
-
-        echo "Logger: I've written '$event' entry to the log.\n";
-    }
-}
-
-/**
- * This Concrete Component sends initial instructions to new users. The client
- * is responsible for attaching this component to a proper user creation event.
- */
-class OnboardingNotification implements \SplObserver
-{
-    private $adminEmail;
-
-    public function __construct($adminEmail)
-    {
-        $this->adminEmail = $adminEmail;
-    }
-
-    public function update(\SplSubject $repository, string $event = null, $data = null): void
-    {
-        // mail($this->adminEmail,
-        //     "Onboarding required",
-        //     "We have a new user. Here's his info: " .json_encode($data));
-
-        echo "OnboardingNotification: The notification has been emailed!\n";
-    }
-}
-
-/**
- * The client code.
- */
-
-$repository = new UserRepository();
-$repository->attach(new Logger(__DIR__ . "/log.txt"), "*");
-$repository->attach(new OnboardingNotification("1@example.com"), "users:created");
-
-$repository->initialize(__DIR__ . "/users.csv");
-
-// ...
-
-$user = $repository->createUser([
-    "name" => "John Smith",
-    "email" => "john99@example.com",
-]);
-
-// ...
-
-$repository->deleteUser($user);
