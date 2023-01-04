b50fd8b9dca56c54a7f6cd7b5dd54591e0362c06
Habibun Noby <habibun.noby@gmail.com>
2023-01-04 14:30:17 +0600

    feat(Migrations): 8.1 fsync and fdatasync

--- src/Migration/Php80To81/Fdatasync.php
+++ src/Migration/Php80To81/Fdatasync.php
@@ -4,5 +4,19 @@ namespace Php\Migration\Php80To81;
 
 class Fdatasync
 {
+    public function test()
+    {
+        $file = 'test.txt';
 
+        $fh = fopen($file, 'w');
+        fwrite($fh, 'test data');
+        fwrite($fh, "\r\n");
+        fwrite($fh, 'additional data');
+
+        fsync($fh);
+        fclose($fh);
+    }
 }
+
+$result = new Fdatasync();
+$result->test();
--- src/Migration/Php80To81/Fsync.php
+++ src/Migration/Php80To81/Fsync.php
@@ -4,5 +4,12 @@ namespace Php\Migration\Php80To81;
 
 class Fsync
 {
-
+    public function test()
+    {
+        $fh = fopen('memory://test');
+        fsync($fh);
+    }
 }
+
+$result = new Fsync();
+$result->test();
