3efe2b657b302080f4ef8a1500839dcb4d512170
Habibun Noby <habibun.noby@gmail.com>
2023-01-04 14:09:36 +0600

    feat(Migrations): 8.1 never return type

--- src/Migration/Php80To81/NeverReturnType.php
+++ src/Migration/Php80To81/NeverReturnType.php
@@ -4,5 +4,13 @@ namespace Php\Migration\Php80To81;
 
 class NeverReturnType
 {
+    function redirect(string $uri): never {
+        header('Location: ' . $uri);
+        exit();
+    }
 
+    function redirectToLoginPage(): never {
+        redirect('/login');
+        echo 'Hello'; // <- dead code detected by static analysis
+    }
 }
