71f2007991a1a041ada0705e14b39af17e9a05a3
Habibun Noby <habibun.noby@gmail.com>
2023-01-04 14:26:38 +0600

    feat(Migrations): 8.1 ReturnTypeWillChange

--- src/Migration/Php80To81/ReturnTypeWillChange.php
+++ src/Migration/Php80To81/ReturnTypeWillChange.php
@@ -4,5 +4,7 @@ namespace Php\Migration\Php80To81;
 
 class ReturnTypeWillChange
 {
-
+//    public function offsetGet(mixed $offset) {}
+    #[\ReturnTypeWillChange]
+    public function offsetGet(mixed $offset): string {}
 }
