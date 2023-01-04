22fadcdee66649a208a761045568456798338444
Habibun Noby <habibun.noby@gmail.com>
2023-01-04 14:05:31 +0600

    feat(Migrations): 8.1 Pure Intersection Types

--- src/Migration/Php80To81/PureIntersectionTypes.php
+++ src/Migration/Php80To81/PureIntersectionTypes.php
@@ -2,7 +2,18 @@
 
 namespace Php\Migration\Php80To81;
 
+use Countable;
+use Iterator;
+
 class PureIntersectionTypes
 {
+    function count_and_iterate(Iterator&Countable $value) {
+        foreach ($value as $val) {
+            echo $val;
+        }
+
+        count($value);
+    }
+
 
 }
